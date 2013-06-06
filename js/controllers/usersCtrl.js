5e133ed73596fb7e0c4446e3bb4c9019a9a8e779
guti6 <abgutman1@gmail.com>
2013-06-05 19:25:00 -0700

    adding all the data in the users.html page

---
+++ js/controllers/usersCtrl.js
@@ -0,0 +1,14 @@
+angular.module('mainApp', ['firebase'])
+  .controller('UserCtrl', ['$scope', 'angularFireCollection',
+    function($scope, angularFireCollection) {
+      
+      var url = 'https://monkey-23.firebaseio-demo.com/users2';
+      $scope.users = angularFireCollection(new Firebase(url));
+      
+
+      $scope.saveNewUser = function(newUser) {
+        $scope.users.add(newUser);
+        $scope.newUser = {'img':''};
+      }
+    }
+  ])
