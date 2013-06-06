261ca7bd5518f04de1ca4f0cd97b072827189dab
guti6 <abgutman1@gmail.com>
2013-06-05 21:11:21 -0700

    make the url be in the query and not in the hash

--- js/controllers/usersCtrl.js
+++ js/controllers/usersCtrl.js
@@ -1,8 +1,17 @@
+// TODO(gutman): move to utils:
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
 angular.module('mainApp', ['firebase'])
-  .controller('UserCtrl', ['$scope', '$location', 'angularFireCollection',
-    function($scope, $location, angularFireCollection) {
+  .controller('UserCtrl', ['$scope', '$location','$routeParams', 'angularFireCollection',
+    function($scope, $location, $routeParams, angularFireCollection) {
       
-      var userId = $location.hash();
+      var userId = getParameterByName('user');
+      debugger;
       var fullUrl = 'https://monkey-23.firebaseio-demo.com/users2/' + userId;
       var messageListRef = new Firebase(fullUrl);
       messageListRef.on('value', function(snapshot) {
