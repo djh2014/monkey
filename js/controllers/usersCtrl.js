34697f90824126097fb6e692a5dfa265a4b9298b
guti6 <abgutman1@gmail.com>
2013-06-05 20:55:39 -0700

    can view in main page, thougu probably wanna go after path and not tag.

--- js/controllers/usersCtrl.js
+++ js/controllers/usersCtrl.js
@@ -1,10 +1,20 @@
 angular.module('mainApp', ['firebase'])
-  .controller('UserCtrl', ['$scope', 'angularFireCollection',
-    function($scope, angularFireCollection) {
+  .controller('UserCtrl', ['$scope', '$location', 'angularFireCollection',
+    function($scope, $location, angularFireCollection) {
       
-      var url = 'https://monkey-23.firebaseio-demo.com/users2';
-      $scope.users = angularFireCollection(new Firebase(url));
-      
+      var userId = $location.hash();
+      var fullUrl = 'https://monkey-23.firebaseio-demo.com/users2/' + userId;
+      var messageListRef = new Firebase(fullUrl);
+      messageListRef.on('value', function(snapshot) {
+        $scope.$apply(function() {
+            $scope.userToView = snapshot.val();
+        });
+      });
+
+      var url = 'https://monkey-23.firebaseio-demo.com/users2/';
+      $scope.users = angularFireCollection(new Firebase(url), function(users) {
+        //debugger;
+      });
 
       $scope.saveNewUser = function(newUser) {
         $scope.users.add(newUser);
