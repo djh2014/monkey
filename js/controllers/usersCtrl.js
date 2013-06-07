a50381057076ffd4eb98bba59edb9ba0f4de5d85
guti6 <abgutman1@gmail.com>
2013-06-06 19:36:26 -0700

    show all people and links seems to be fine, just need more data

--- js/controllers/usersCtrl.js
+++ js/controllers/usersCtrl.js
@@ -13,17 +13,30 @@ angular.module('mainApp', ['firebase'])
       var userId = getParameterByName('user');
       var fullUrl = 'https://monkey-23.firebaseio-demo.com/users2/' + userId;
       var messageListRef = new Firebase(fullUrl);
+      $scope.usersToView = [];
+
       messageListRef.on('value', function(snapshot) {
         $scope.$apply(function() {
             // For details.html page:
             $scope.userToView = snapshot.val();
             // For index.html page:
             debugger;
-            $scope.users = snapshot.val();
-            var ids = Object.keys($scope.users);
+            var users = [];
 
-            $scope.user1 = $scope.users[ids[0]];
-            $scope.user1.id = ids[0];
+            var BIG_IMG = [600, 450, 6], MEDIUM_IMG = [260, 200, 3], SMALL_IMG = [225, 200, 3];
+            var IMG_SIZES = [BIG_IMG, MEDIUM_IMG, MEDIUM_IMG, MEDIUM_IMG, MEDIUM_IMG, BIG_IMG, SMALL_IMG, SMALL_IMG, SMALL_IMG, SMALL_IMG];
+            var usersObject = snapshot.val();
+            var ids = Object.keys(usersObject);
+
+            for (var i = 0; i < ids.length && i< IMG_SIZES.length; i++) {
+              var user = usersObject[ids[i]];
+              user.imgWidth = IMG_SIZES[i][0];
+              user.imgHeight = IMG_SIZES[i][1];
+              user.spanSize = IMG_SIZES[i][2];
+              user.id = ids[i];
+              users.push(user);
+            };
+            $scope.usersToView = users; 
         });
       });
 
