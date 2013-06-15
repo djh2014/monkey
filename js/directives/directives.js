7ef2a48409f6c7e8e7dadd4edda3a1f7dff81ebc
guti6 <abgutman1@gmail.com>
2013-06-15 12:07:11 -0700

    basic navbar can figure out how it work, maybe later

--- js/directives/directives.js
+++ js/directives/directives.js
@@ -1,6 +1,6 @@
 mainApp.directive('profile', function() {
     return {
-      template: '<img src="https://i.embed.ly/1/display/resize?key=dc65793b3f1249bdb9952a491874d27e&url={{user.img}}&width={{width}}&height={{height}}&grow=true" title="{{user.name}}"/>',
+      template: '<a href="/#/detail/{{user.id}}"><img src="https://i.embed.ly/1/display/resize?key=dc65793b3f1249bdb9952a491874d27e&url={{user.img}}&width={{width}}&height={{height}}&grow=true" title="{{user.name}}"/></a>',
       replace: true,
       restrict: 'E',
       scope: { user: '=', currentUser: '='}, 
