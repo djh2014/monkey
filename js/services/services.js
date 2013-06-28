292c0c7f5cd82649cbd96a4a8daab7825df0a5fb
Danny <dannyjhaber@gmail.com>
2013-06-28 10:36:10 -0700

    updates

--- js/services/services.js
+++ js/services/services.js
@@ -119,6 +119,16 @@ mainApp
   	  return scope.$$phase || scope.$apply();;
   	},
 
+  	randomizeArray : function(myArray) {
+	  var i = myArray.length, j, temp;
+	  if ( i === 0 ) return false;
+	  while ( --i ) {
+	     j = Math.floor( Math.random() * ( i + 1 ) );
+	     temp = myArray[i];
+	     myArray[i] = myArray[j]; 
+	     myArray[j] = temp;
+	   }
+	},
 
   	log : function(event, extra) {
   		try
